bcc85d9303f8216161c85ecfec1fdffe4235fdc6
Jeroen Cranendonk <jeroen.cranendonk@gmail.com>
2025-02-20 20:30:09 +0800

    Update credentials.ts

--- src/credentials.ts
+++ src/credentials.ts
@@ -1,14 +1,21 @@
+import { TOTP } from "totp-generator"
+
 /**
  * Pulls username, password and OTP code from env vars
  */
 export const getCredentials = async () => {
   const user = process.env["AMAZON_USER"];
   const password = process.env["PASSWORD"];
-  const otp = process.env["OTP"];
+  const otpKey = process.env["TOTP"];
 
   if (!user || !password) {
     throw new Error("Failed to get login credentials from env");
   }
 
-  return { user, password, otp };
+  if (otpKey) {
+    const { otp } = TOTP.generate(otpKey);
+    return { user, password, otp };
+  }
+
+  return { user, password };
 };
